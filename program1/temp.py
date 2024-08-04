42290dfe246cbc3e606e10a1efb62df15e579242
yuvrajgusain262 <yuvrajgusain66@gmail.com>
2024-08-04 23:15:59 +0530

    Create temp.py

---
+++ program1/temp.py
@@ -0,0 +1,27 @@
+import random
+import time
+from datetime import datetime
+import json
+
+# Function to generate random temperature and humidity data
+def generate_data():
+    temperature = round(random.uniform(20, 40))
+    humidity = round(random.uniform(0, 10))
+    return {
+        'Temperature': temperature,
+        'Humidity': humidity,
+        'Time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    }
+
+# Function to generate and save JSON data periodically
+def generate_json():
+    data_freq = 10
+    data = []
+    interval = 5
+    for i in range(data_freq):
+        data_generated = generate_data()
+        data.append(data_generated)
+        time.sleep(interval)
+    # Save JSON data to file
+    with open(r'D:\python\iot blockchain\data.json', 'w') as f:
+        json.dump(data, f, indent=4)
